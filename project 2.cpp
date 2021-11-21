e9f2a2f0fb16bdbdd63a65ffecf46d5cbc4812eb
samar03-gif <94772832+samar03-gif@users.noreply.github.com>
2021-11-21 09:38:00 +0530

    Add files via upload

---
+++ project 2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, product;
+    cout <<"enter the value of num1: ";
+    cin >> num1;
+    cout <<"enter the value of num2: ";
+    cin >> num2;
+    product = num1*num2;
+    cout<<"the product is: "<<product;
+    return 0;
+}
